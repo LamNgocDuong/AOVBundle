ec019c5462981db4224aae4c775fc67a396152b9
LamNgocDuong <lamduongngoc0610@gmail.com>
2024-11-17 09:41:30 +0700

    Update testcase

--- cypress/e2e/ui/frequentlyboughttogether.cy.js
+++ cypress/e2e/ui/frequentlyboughttogether.cy.js
@@ -1,16 +1,45 @@
 import {loginStore} from "../../support/loginUtils";
 import {STORE_PASS, STORE_URL} from "../../support/config";
 
-describe('Check cart after adding', () => {
-  it('FBT', () => {
-    loginStore(STORE_URL, STORE_PASS )
+describe('Frequently bought together', () => {
+  it('Check display inline frequently bought together', () => {
+    loginStore(STORE_URL, STORE_PASS)
+    // Visit product page
+    cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
+
+    // Check class visibility
+    cy.get('.Avada-Bundle-Branding-Container')
+        // Kiểm tra xem nó có hiển thị không
+        // Cuộn đến phần tử
+        .scrollIntoView()
+        .then(($element) => {
+          // Kiểm tra xem phần tử có tồn tại không
+          if ($element.length > 0) {
+            cy.log('AOV-Branding tồn tại');
+            console.log('AOV-Branding tồn tại');
+
+            // Kiểm tra phần tử có hiển thị
+            cy.wrap($element).should('be.visible');
+            cy.log('Class Avada-Bundle-Branding-Container đang hiển thị');
+            console.log('Class Avada-Bundle-Branding-Container đang hiển thị');
+          } else {
+            // In ra console nếu phần tử không tồn tại
+            cy.log('Class Avada-Bundle-Branding-Container không tồn tại');
+            console.log('Class Avada-Bundle-Branding-Container không tồn tại');
+          }
+        });
+  });
+
+
+  it('Check item add to cart', () => {
+    loginStore(STORE_URL, STORE_PASS)
 
     //check product show FBT
     cy.get('#Slide-template--16350353981517__featured-collection-1 > div > div').click()
 
     //check add to cart and value total price
     cy.get('.Avada-Offer__ButtonAddToCart').click()
-    cy.get('.totals__total-value').should('have.text','2.934 VND')
+    cy.get('.totals__total-value').should('have.text', '2.934 VND')
 
     //close cart drawer
     cy.get('#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg')
@@ -20,10 +49,26 @@ describe('Check cart after adding', () => {
     cy.wait(4000);
     cy.get('button').contains('ITEM ADDED TO YOUR CART').should('be.visible');
   })
-})
 
-describe('Check untick product', () => {
-  it('FBT', () => {
+
+  it('Check cart after unticking product offer', () => {
     loginStore(STORE_URL, STORE_PASS)
+    cy.get('#Slide-template--16350353981517__featured-collection-1 > div > div').click()
+    //untick product offer
+    cy.get('#shopify-block-aov_bundle_upsell_offer_inline_RUaeyp > div > div > div > div > div > div > div.Avada-Fbt__WrapProductFooter > div.Avada-Fbt__ProductList > div:nth-child(2) > div.Avada-OfferItem__Checkbox > div > div')
+        .click()
+    cy.get('.Avada-Offer__ButtonAddToCart').click()
+    cy.get('.totals__total-value').should('have.text', '2.630 VND')
+  })
+
+  it('Check cart after unticking product trigger', () => {
+    loginStore(STORE_URL, STORE_PASS)
+    cy.get('#Slide-template--16350353981517__featured-collection-1 > div > div').click()
+    //untick product trigger
+    cy.get('#shopify-block-aov_bundle_upsell_offer_inline_RUaeyp > div > div > div > div > div > div > div.Avada-Fbt__WrapProductFooter > div.Avada-Fbt__ProductList > div:nth-child(1) > div.Avada-OfferItem__Checkbox > div')
+        .click()
+    cy.get('.Avada-Offer__ButtonAddToCart').click()
+    cy.get('.totals__total-value').should('have.text', '630 VND')
+
   })
 })
