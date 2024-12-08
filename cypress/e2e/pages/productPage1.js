131620797c8fb3ed4ebf25932cf7c87bf9fce0d0
LamNgocDuong <lamduongngoc0610@gmail.com>
2024-12-08 16:02:36 +0700

    Add project object pages and update testcase

---
+++ cypress/e2e/pages/productPage1.js
@@ -0,0 +1,21 @@
+const BRANDING_AOV = '.Avada-Bundle-Branding-Container'
+export const ADDTOCART = '.Avada-Offer__ButtonAddToCart ';
+export const CHECK_BOX = ".Avada-Checkbox__Wrapper"
+export const TOTAL_VALUE = ".totals__total-value"
+
+class  productPage1 {
+    get brandingAOVBundle() {
+        return cy.get(BRANDING_AOV)
+    }
+    get clickAddToCart() {
+        return cy.get(ADDTOCART).click()
+    }
+    get checkTotalValue() {
+        return cy.get(TOTAL_VALUE)
+    }
+    get checkBox() {
+        return cy.get(CHECK_BOX)
+    }
+
+}
+module.export = productPage1;
--- cypress/e2e/ui/frequentlyboughttogether.cy.js
+++ cypress/e2e/ui/frequentlyboughttogether.cy.js
@@ -1,110 +1,89 @@
 import {loginStore} from "../../support/loginUtils";
 import {ADDTOCART, CHECK_BOX, REMOVE_PRODUCT, STORE_PASS, STORE_URL, TOTAL_VALUE} from "../../support/config";
 
+const closeCartDrawn = '#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg'
+
 describe('Frequently bought together', () => {
   it('Check display inline frequently bought together with trigger condition is specific product', () => {
     loginStore(STORE_URL, STORE_PASS)
     // Visit product page
     cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
 
-    // Check class visibility
-    cy.get('.Avada-Bundle-Branding-Container')
-        // Cuộn đến phần tử
-        .scrollIntoView()
-        .then(($element) => {
-          // Kiểm tra xem phần tử có tồn tại không
-          if ($element.length > 0) {
-            cy.log('AOV-Branding tồn tại');
-            console.log('AOV-Branding tồn tại');
-
-            // Kiểm tra phần tử có hiển thị
-            cy.wrap($element).should('be.visible');
-            cy.log('Class Avada-Bundle-Branding-Container đang hiển thị');
-            console.log('Class Avada-Bundle-Branding-Container đang hiển thị');
-          } else {
-            // In ra console nếu phần tử không tồn tại
-            cy.log('Class Avada-Bundle-Branding-Container không tồn tại');
-            console.log('Class Avada-Bundle-Branding-Container không tồn tại');
-          }
-        })
+    let prodPage1 = new productPage1()
+      prodPage1.brandingAOVBundle.should('have.class', 'active')
   });
-
 
   it('Check add to cart', () => {
       loginStore(STORE_URL, STORE_PASS)
-
-      //check product show FBT
-      cy.get('#Slide-template--16350353981517__featured-collection-1 > div > div').click()
-
+      cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
       //check add to cart and value total price
-      cy.get(ADDTOCART).click()
-      cy.wait(4000)
-      cy.get(TOTAL_VALUE).should('have.text', '3.455 VND')
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.clickAddToCart
+      prodPage1.checkTotalValue.should('have.text', '3.455 VND')
 
       //close cart drawer
-      cy.get('#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg')
-          .click()
+      cy.get(closeCartDrawn).click()
 
       // After clicking, check that the text changes to "ITEM ADDED TO YOUR CART"
-      cy.wait(4000);
-      cy.get('button').contains('ITEM ADDED TO YOUR CART').should('be.visible');
+      cy.get('button').contains('ITEM ADDED TO YOUR CART')
+          .should('be.visible');
     })
 
 
   it('Check cart after untick product offer', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.get('#Slide-template--16350353981517__featured-collection-1 > div > div').click()
+      cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
       //untick product offer
-      cy.get(CHECK_BOX).eq(1).click()
-      cy.get(ADDTOCART).click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '2.630 VND')
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.checkBox.eq(1).click()
+      prodPage1.clickAddToCart
+      prodPage1.checkTotalValue.should('have.text', '3.965 VND')
     })
 
   it('Check cart after unticking product trigger', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.get('#Slide-template--16350353981517__featured-collection-1 > div > div').click()
+      ccy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
       //untick product trigger
-      cy.get(CHECK_BOX).eq(0).click()
-      cy.get(ADDTOCART).click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '1.025 VND')
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.checkBox.eq(1).click()
+      prodPage1.clickAddToCart
+      prodPage1.checkTotalValue
+          .should('have.text', '2.375 VND')
     })
 
   it('Check delete product offer in cart', () => {
       loginStore(STORE_URL, STORE_PASS)
       cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
-      //check add to cart and value total price
-      cy.get(ADDTOCART).click()
+
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.clickAddToCart
       // delete product
-      cy.get('#CartDrawer-Remove-1 > button').click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '2.630 VND')
+      cy.get('#CartDrawer-Remove-2').click()
+      prodPage1.checkTotalValue
+          .should('have.text', '3.582 VND')
     })
 
   it('Check delete product trigger in cart', () => {
       loginStore(STORE_URL, STORE_PASS)
       cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard');
-      //check add to cart and value total price
-      cy.get(ADDTOCART).click()
+
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.clickAddToCart
       // delete product
-      cy.wait(4000);
-      cy.get('#CartDrawer-Remove-2 > button').click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '1.025 VND')
+      cy.get('#CartDrawer-Remove-1').click()
+      prodPage1.checkTotalValue
+          .should('have.text', '2.375 VND')
     })
 
   it('Check display inline frequently bought together with trigger condition is specific collection', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.visit(STORE_URL + 'products/the-collection-snowboard-hydrogen');
-      cy.wait(2000)
-      cy.get(ADDTOCART).click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '1.425 VND')
+      cy.visit(STORE_URL + 'products/the-archived-snowboard');
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.clickAddToCart
+      prodPage1.checkTotalValue.should('have.text', '1.425 VND')
 
       //close cart drawer
-      cy.get('#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg')
-          .click()
+      cy.get(closeCartDrawn).click()
 
       // After clicking, check that the text changes to "ITEM ADDED TO YOUR CART"
       cy.wait(4000);
@@ -113,39 +92,37 @@ describe('Frequently bought together', () => {
 
   it('Check untick product offer', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.visit(STORE_URL + 'products/the-collection-snowboard-hydrogen');
-      cy.wait(2000)
-      cy.get(CHECK_BOX).eq(1).click()
-      cy.get(ADDTOCART).click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '600 VND')
+      cy.visit(STORE_URL + 'products/the-archived-snowboard');
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.checkBox.eq(1).click()
+      prodPage1.clickAddToCart
+      prodPage1.checkTotalValue.should('have.text', '630 VND')
     })
 
   it('Check untick product trigger', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.visit(STORE_URL + 'products/the-collection-snowboard-hydrogen');
-      cy.wait(2000)
-      cy.get(CHECK_BOX).eq(0).click()
-      cy.get(ADDTOCART).click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '1.025 VND')
+      cy.visit(STORE_URL + 'products/the-archived-snowboard');
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.checkBox.eq(0).click()
+      prodPage1.clickAddToCart
+      prodPage1.checkTotalValue.should('have.text', '1.025 VND')
     })
 
   it('Check delete product offer in cart', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.visit(STORE_URL + 'products/the-collection-snowboard-hydrogen');
-      cy.get(ADDTOCART).click()
-      cy.get('#CartDrawer-Remove-1 > button').click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '600 VND')
+      cy.visit(STORE_URL + 'products/the-archived-snowboard');
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.clickAddToCart
+      cy.get('#CartDrawer-Remove-1').click()
+      prodPage1.checkTotalValue.should('have.text', '630 VND')
     })
 
   it('Check delete product trigger in cart', () => {
       loginStore(STORE_URL, STORE_PASS)
-      cy.visit(STORE_URL + 'products/the-collection-snowboard-hydrogen');
-      cy.get(ADDTOCART).click()
-      cy.get('#CartDrawer-Remove-2 > button').click()
-      cy.wait(4000);
-      cy.get(TOTAL_VALUE).should('have.text', '1.025 VND')
+      cy.visit(STORE_URL + 'products/the-archived-snowboard');
+      let prodPage1 = new prodcuctPage1()
+      prodPage1.clickAddToCart
+      cy.get('#CartDrawer-Remove-2').click()
+      prodPage1.checkTotalValue.should('have.text', '1.025 VND')
     })
 })
