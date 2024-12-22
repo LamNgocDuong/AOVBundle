615804a1f201b335bfa7d66b02d5c9c747045d3e
LamNgocDuong <lamduongngoc0610@gmail.com>
2024-12-22 10:54:37 +0700

    Refactor code

--- cypress/e2e/ui/fbtTriggerProduct.cy.js
+++ cypress/e2e/ui/fbtTriggerProduct.cy.js
@@ -2,66 +2,57 @@ import {loginStore} from "../../support/loginUtils";
 import { STORE_PASS, STORE_URL} from "../../support/config";
 import  productPage  from '../selector/productPage';
 
-const closeCartDrawn = '#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg'
+const PRODUCT_URL = 'products/the-3p-fulfilled-snowboard';
+const CART_DRAWER_CLOSE = '#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg';
+const CART_REMOVE_BUTTONS = {
+  FIRST_ITEM: '#CartDrawer-Remove-1 > button',
+  FOURTH_ITEM: '#CartDrawer-Remove-4'
+};
 
 describe('Frequently bought together', () => {
+    let prodPage;
 
-    beforeEach(() =>{
-        loginStore(STORE_URL, STORE_PASS)
-        cy.visit(STORE_URL + 'products/the-3p-fulfilled-snowboard')
-    })
+    beforeEach(() => {
+        loginStore(STORE_URL, STORE_PASS);
+        cy.visit(STORE_URL + PRODUCT_URL);
+        prodPage = new productPage();
+    });
 
     it('Check add to cart', () => {
+        prodPage.clickAddToCart();
+        prodPage.checkTotalValue.should('have.text', '4.505 VND');
 
-        //check add to cart and value total price
-        let prodPage = new productPage()
-            prodPage.clickAddToCart
-            prodPage.checkTotalValue.should('have.text', '4.505 VND')
-
-        //close cart drawer
-        cy.get(closeCartDrawn).should('be.visible').click()
-
-        // After clicking, check that the text changes to "ITEM ADDED TO YOUR CART"
-        cy.get('button').contains('ITEM ADDED TO YOUR CART')
-            .should('be.visible');
-    })
-
+        cy.get(CART_DRAWER_CLOSE)
+          .should('be.visible')
+          .click();
+        
+        cy.get('button')
+          .contains('ITEM ADDED TO YOUR CART')
+          .should('be.visible');
+    });
 
     it('Check cart after untick product offer', () => {
-
-        //untick product offer
-        let prodPage = new productPage()
-            prodPage.checkBox.eq(1).click()
-            prodPage.clickAddToCart
-            prodPage.checkTotalValue.should('contain.text', '3.965 VND')
-    })
+        prodPage.checkBox.eq(1).click();
+        prodPage.clickAddToCart();
+        prodPage.checkTotalValue.should('contain.text', '3.965 VND');
+    });
 
     it('Check cart after unticking product trigger', () => {
-
-        //untick product trigger
-        let prodPage = new productPage()
-            prodPage.checkBox.eq(0).click()
-            prodPage.clickAddToCart
-            prodPage.checkTotalValue
-                .should('contain.text', '2.375 VND')
-    })
+        prodPage.checkBox.eq(0).click();
+        prodPage.clickAddToCart();
+        prodPage.checkTotalValue
+            .should('contain.text', '2.375 VND');
+    });
 
     it('Check delete product offer in cart', () => {
-
-        let prodPage = new productPage()
-            prodPage.clickAddToCart
-        // delete product
-        cy.get('#CartDrawer-Remove-1 > button').click()
-            prodPage.checkTotalValue.should('contain.text', '3.582 VND')
-    })
+        prodPage.clickAddToCart();
+        cy.get(CART_REMOVE_BUTTONS.FIRST_ITEM).click();
+        prodPage.checkTotalValue.should('contain.text', '3.582 VND');
+    });
 
     it('Check delete product trigger in cart', () => {
-
-        let prodPage = new productPage()
-            prodPage.clickAddToCart
-        // delete product
-        cy.get('#CartDrawer-Remove-4').click()
-            prodPage.checkTotalValue.should('contain.text', '2.375 VND')
-    })
-
-})
+        prodPage.clickAddToCart();
+        cy.get(CART_REMOVE_BUTTONS.FOURTH_ITEM).click();
+        prodPage.checkTotalValue.should('contain.text', '2.375 VND');
+    });
+});
