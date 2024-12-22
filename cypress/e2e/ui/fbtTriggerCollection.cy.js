cafaa45eedcf4c06c1df36834b34338991baca8d
LamNgocDuong <lamduongngoc0610@gmail.com>
2024-12-22 10:53:25 +0700

    Refactor code

--- cypress/e2e/ui/fbtTriggerCollection.cy.js
+++ cypress/e2e/ui/fbtTriggerCollection.cy.js
@@ -2,53 +2,53 @@ import {loginStore} from "../../support/loginUtils";
 import { STORE_PASS, STORE_URL} from "../../support/config";
 import productPage from '../selector/productPage';
 
-const closeCartDrawn = '#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg'
+const PRODUCT_URL = 'products/the-archived-snowboard';
+const CART_DRAWER_CLOSE = '#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg';
+const CART_REMOVE_BUTTONS = {
+  FIRST_ITEM: '#CartDrawer-Remove-1',
+  SECOND_ITEM: '#CartDrawer-Remove-2'
+};
 
 describe('Frequently bought together', () => {
+    let prodPage;
 
-    beforeEach(() =>{
-        loginStore(STORE_URL, STORE_PASS)
-        cy.visit(STORE_URL + 'products/the-archived-snowboard');
-    })
+    beforeEach(() => {
+        loginStore(STORE_URL, STORE_PASS);
+        cy.visit(STORE_URL + PRODUCT_URL);
+        prodPage = new productPage();
+    });
 
     it('Check display inline frequently bought together with trigger condition is specific collection', () => {
+        prodPage.clickAddToCart();
+        prodPage.checkTotalValue.should('have.text', '1.455 VND');
 
-       let prodPage = new productPage()
-           prodPage.clickAddToCart
-           prodPage.checkTotalValue.should('have.text', '1.455 VND')
-
-        //close cart drawer
-        cy.get(closeCartDrawn).click()
-
-        // After clicking, check that the text changes to "ITEM ADDED TO YOUR CART"
-        cy.get('button').contains('ITEM ADDED TO YOUR CART').should('be.visible');
+        cy.get(CART_DRAWER_CLOSE).click();
+        cy.get('button')
+          .contains('ITEM ADDED TO YOUR CART')
+          .should('be.visible');
     });
 
     it('Check untick product offer', () => {
-       let prodPage = new productPage()
-           prodPage.checkBox.eq(1).click()
-           prodPage.clickAddToCart
-           prodPage.checkTotalValue.should('have.text', '630 VND')
+       prodPage.checkBox.eq(1).click()
+       prodPage.clickAddToCart()
+       prodPage.checkTotalValue.should('have.text', '630 VND')
     })
 
     it('Check untick product trigger', () => {
-       let prodPage = new productPage()
-           prodPage.checkBox.eq(0).click()
-           prodPage.clickAddToCart
-           prodPage.checkTotalValue.should('have.text', '1.025 VND')
+       prodPage.checkBox.eq(0).click()
+       prodPage.clickAddToCart()
+       prodPage.checkTotalValue.should('have.text', '1.025 VND')
     })
 
     it('Check delete product offer in cart', () => {
-       let prodPage = new productPage()
-           prodPage.clickAddToCart
-       cy.get('#CartDrawer-Remove-1').click()
+       prodPage.clickAddToCart()
+       cy.get(CART_REMOVE_BUTTONS.FIRST_ITEM).click()
            prodPage.checkTotalValue.should('have.text', '630 VND')
     })
 
     it('Check delete product trigger in cart', () => {
-       let prodPage = new productPage()
-           prodPage.clickAddToCart
-       cy.get('#CartDrawer-Remove-2').click()
+       prodPage.clickAddToCart()
+       cy.get(CART_REMOVE_BUTTONS.SECOND_ITEM).click()
            prodPage.checkTotalValue.should('have.text', '1.025 VND')
     })
 })
