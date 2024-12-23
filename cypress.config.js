efc601626ef1c1917f9c40cc1fc5e60109b6d161
thomasavada <anhnt@avada.io>
2024-12-23 10:46:43 +0700

    Fix tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  browser: 'chrome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     defaultCommandTimeout: 10000
   },
-
-  retries: {
-    experimentalStrategy: 'detect-flake-and-pass-on-threshold',
-    experimentalOptions: {
-      maxRetries: 2,
-      passesRequired: 2,
-    },
-
-    openMode: true,
-    runMode: true,
-  },
+  // retries: {
+  //   experimentalStrategy: 'detect-flake-and-pass-on-threshold',
+  //   experimentalOptions: {
+  //     maxRetries: 2,
+  //     passesRequired: 2,
+  //   },
+  //
+  //   openMode: true,
+  //   runMode: true,
+  // },
+  "userAgent": "'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'",
+  chromeWebSecurity: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080
 });
--- cypress/e2e/ui/fbtTriggerProduct.cy.js
+++ cypress/e2e/ui/fbtTriggerProduct.cy.js
@@ -1,34 +1,34 @@
 import {loginStore} from "../../support/loginUtils";
-import { STORE_PASS, STORE_URL} from "../../support/config";
-import  productPage  from '../selector/productPage';
+import {STORE_PASS, STORE_URL} from "../../support/config";
+import ProductPage from '../selector/productPage';
 
 const PRODUCT_URL = 'products/the-3p-fulfilled-snowboard';
 const CART_DRAWER_CLOSE = '#CartDrawer > div.drawer__inner.gradient.color-scheme-1 > div.drawer__header > button > span > svg';
 const CART_REMOVE_BUTTONS = {
-  FIRST_ITEM: '#CartDrawer-Remove-1 > button',
-  FOURTH_ITEM: '#CartDrawer-Remove-4'
+    FIRST_ITEM: '#CartDrawer-Remove-1 > button',
+    FOURTH_ITEM: '#CartDrawer-Remove-4'
 };
 
+const prodPage = new ProductPage();
+
 describe('Frequently bought together', () => {
-    let prodPage;
 
     beforeEach(() => {
         loginStore(STORE_URL, STORE_PASS);
         cy.visit(STORE_URL + PRODUCT_URL);
-        prodPage = new productPage();
-    });
+    })
 
     it('Check add to cart', () => {
         prodPage.addToCart();
         prodPage.totalValue().should('have.text', '4.505 VND');
 
         cy.get(CART_DRAWER_CLOSE)
-          .should('be.visible')
-          .click();
-        
+            .should('be.visible')
+            .click();
+
         cy.get('button')
-          .contains('ITEM ADDED TO YOUR CART')
-          .should('be.visible');
+            .contains('ITEM ADDED TO YOUR CART')
+            .should('be.visible');
     });
 
     it('Check cart after untick product offer', () => {
