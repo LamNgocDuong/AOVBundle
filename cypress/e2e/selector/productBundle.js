33b23a03ff8948ace899aac5f6f91bb9e6502e45
lamduongngoc <lamduongngoc0610@gmail.com>
2025-01-08 17:22:41 +0700

    create testcase bundle

--- cypress/e2e/selector/productBundle.js
+++ cypress/e2e/selector/productBundle.js
@@ -1,4 +1,19 @@
-class productBundle{
+const SELECTORS = {
+    ADD_TO_CART: '.product-form__buttons',
+    TOTAL_VALUE: '.cart-item__price-wrapper',
+    CHECK_OUT: '.cart__ctas'
 
 }
-module.exports = productBundle;
+
+class ProductBundle{
+    addToCart(){
+        return cy.get(SELECTORS.ADD_TO_CART).click();
+    }
+    totalValue(){
+        return cy.get(SELECTORS.TOTAL_VALUE);
+    }
+    checkOut(){
+        return cy.get(SELECTORS.CHECK_OUT).click();
+    }
+}
+export default ProductBundle;
--- cypress/e2e/ui/productFixedBundle.cy.js
+++ cypress/e2e/ui/productFixedBundle.cy.js
@@ -1,12 +1,33 @@
 import {loginStore} from "../../support/loginUtils";
 import {STORE_URL} from "../../support/config";
+import ProductBundle from '../selector/productBundle';
+
+
+const BUNDLE_NAME = '.product__title';
+const prodBundle = new ProductBundle();
 
 describe('Product fixed bundle', () => {
     beforeEach(() => {
         loginStore(STORE_URL, STORE_URL);
         cy.visit(STORE_URL + "products/product-fixed-bundle-2");
     })
-    it('Check display inline bundle', () => {
+    it('Check product name', () => {
+        prodBundle.BUNDLE_NAME.should('have.text', 'Product Bundle');
+    })
 
+    it('Check add to cart', () => {
+        prodBundles
+    })
+
+    it('Check price product bundle', () => {
+        prodBundle
+    })
+
+    it('Check check out bundle', () => {
+        prodBundle
+    })
+
+    it('Change variant and price product bundle', () => {
+        prodBundle
     })
 });
