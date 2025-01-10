c837706778d420447d4df363207091a17ca1f9b5
lamduongngoc <lamduongngoc0610@gmail.com>
2025-01-10 16:46:21 +0700

    update testcase Bundle

--- cypress/e2e/ui/productFixedBundle.cy.js
+++ cypress/e2e/ui/productFixedBundle.cy.js
@@ -26,7 +26,7 @@ describe('Product fixed bundle', () => {
         prodBundle.totalValue().should('have.text','3.430 VND');
     })
 
-    it.only('Check display variant in cart', () => {
+    it('Check display variant in cart', () => {
         prodBundle.selectVariant(0, 1); 
         prodBundle.selectVariant(1, 2); 
         prodBundle.addToCart();
@@ -48,7 +48,9 @@ describe('Product fixed bundle', () => {
         prodBundle.totalValue().should('have.text','3.260 VND')
     })
 
-    it('Check checkout bundle process', () => {
+    it.only('Check checkout bundle process', () => {
+        prodBundle.selectVariant(0, 1); 
+        prodBundle.selectVariant(1, 2); 
         prodBundle.addToCart();
         prodBundle.checkOut();
         
@@ -56,8 +58,9 @@ describe('Product fixed bundle', () => {
         cy.url().should('include', '/checkout');
         
         // Verify bundle items are present in checkout
-        prodBundle.getBundleItems()
-            .should('be.visible')
-            .and('have.length.at.least', 1);
+        cy.get('#app > div:nth-child(1) > div > div.cm5pp.U3Rye.FeQiM.oYrwu._1fragemna._1fragemn6._1fragemt0 > div.Sxi8I > div.i4DWM._1fragemna._1fragemn7._1fragemt0 > div > aside > div > section > div > div:nth-child(2) > section > div._6zbcq54._6zbcq53._1fragem28._1fragemnn._6zbcq5m._6zbcq5a._1fragem3w._6zbcq5s > div._6zbcq513._6zbcq512._1fragem28._1fragemnn._6zbcq5m._6zbcq5a._1fragem3w > div > div:nth-child(4) > div > span')
+            .should('have.text','₫3,260');
+        cy.get('#collapsed-components > div > div > div:nth-child(1) > div._6zbcq51n._6zbcq51m._1fragem28._1fragemnn._6zbcq51i._6zbcq51f._1fragem6t._6zbcq51c._6zbcq51k._1fragemnq > span._19gi7yt0._19gi7ytu._19gi7ytt._1fragemnv._19gi7ytj')
+            .should('have.text','Bzone / S')
     })
 });
